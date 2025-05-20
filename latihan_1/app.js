2717e8d33950b6f6f4dacb50cdc275e7ce0623ad
programlia <programlia2901@gmail.com>
2025-05-20 15:07:00 +0700

    menambahkan materi baru

--- latihan_1/app.js
+++ latihan_1/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { title } = require('process');
 
 const app = express();
 
@@ -11,22 +12,80 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.put('/', (req,res) => {
+// menampilkan hello world...
+// get (path , (request , response))...
+app.get("/" , (_,res) => {
+    // method send akan mengembalikan string...
+    res.send("Hello World");
+})
+
+app.get("/test" , (_,res) => {
+    // method json akan mengembalikan json...
     res.json({
-        name:"kjhkjj",
-    })
+        text: "Hello World",
+    });
 })
 
-app.get(`/product/:id` , (req,res) => {
-    const {id, username} = req.params;
-    res.send(`dari user : ke- ${id} dengan username : ${username}`)
+// method pada express ada GET POST PUT DELETE...
+// GET
+app.get("/todos" , (_,res) => {
+    res.json({
+        todos: [
+            {title: 'Makan' , desc: 'Makan bang'},
+            {title: 'Minum' , desc: 'Minum bang'},
+            {title: 'Tidur' , desc: 'Tidur bang'},
+        ]
+    });
 })
 
-app.get(`/product` , (req,res) => {
-    const {categoryid} = req.query;
-    res.send(`dari user : ke- ${categoryid}`)
+// POST
+app.post("/todos" , (_,res) => {
+    res.send("Todo created");
 })
 
+// PUT
+app.put("/todos" , (_,res) => {
+    res.send("Todo updated");
+})
+
+// DELETE
+app.delete("/todos" , (_,res) => {
+    res.send("Todo deleted");
+})
+
+// Dinamis Routes...\
+// untuk route dinamis gunakan :params(bebas namanya)...
+// lalu panggil dengan req.params.id...
+app.get(`/todos/:id` , (req,res) => {
+    // lakukan destruck agar lebih mudah mengakses...
+    const {id} = req.params;
+    res.send(`Ini adalah product dengan ID : ${id}`)
+})
+
+// menggunakan query params...
+app.get(`/products` , (req,res) => {
+    // endPoint akan menjadi seperti ini localhost:3000/products?category=kesehatan
+    const {id} = req.query;
+    const products = [
+            {id: 1 , name:"Rokok" },
+            {id: 2 , name:"Sabun" },
+            {id: 3 , name:"Mobil" },
+            {id: 4 , name:"Qur'an" }
+        ]
+    if(id) {
+        const data = products.filter(item => item.id == id);
+        if(data.length > 0) {
+            res.json({data})
+        }else {
+            res.json({
+                status: "error",
+                message: `Product dengan ID : ${id} belum tersedia`
+            })
+        }
+    }else {
+        res.json({products})
+    }
+})
 
 
 module.exports = app;
